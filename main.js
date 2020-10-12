42a9e367b8624e7de1b68b1a491aee7294ef1b5c
stralth <kinken1992@hotmail.com>
2020-10-12 11:00:14 +0800

    Added check for surrounding click to be ignored if not close to black or white

--- main.js
+++ main.js
@@ -104,48 +104,18 @@ $(".box").click(function(){
     let col = Number(loc[1]);
     let checkRow = [-1,0,1,1,1,0,-1,-1];
     let checkCol = [-1,-1,-1,0,1,1,1,0];
-    let count = 0;  
+    let count = 0;
+    let surround = [];
+
+    for(i=0;i<checkRow.length;i++){
+        surround.push([row+checkRow[i],col+checkCol[i]]);
+    }
+
     if(current_player && !isOccupied){
-        grid[row][col] = 1;
-        for(i=0;i<8;i++){
-            if(row<grid.length && row>-1 && col<grid.length && col>-1){
-            count = 0;
-            chRow = checkRow[i];
-            chCol = checkCol[i];
-            let newRow = 0;
-            let newCol = 0;
-            newRow = row + chRow;
-            newCol = col + chCol;
-                for(j=0;j<grid.length;j++){
-                    if(newRow<grid.length && newRow>-1 && newCol<grid.length && newCol>-1){
-                        if(grid[newRow][newCol] == 2){
-                            newRow += chRow;
-                            newCol += chCol;
-                            count++;
-                        } else if(grid[newRow][newCol] == 1){ 
-                            if(count != 0){
-                                let nRow = newRow;
-                                let nCol = newCol;
-                                for(x=0;x<count;x++){
-                                    nRow -= chRow;
-                                    nCol -= chCol;
-                                    grid[nRow][nCol] = 1;
-                                }
-                            }   
-                        } else if(grid[newRow][newCol] == 0){
-                            break;
-                        }
-                    }
-                    checkColor();
-                }
-            }   
-        }
-        checkColor();
-        changePlayer();
-    } else if (!current_player && !isOccupied) {
-        grid[row][col] = 2;
-        for(i=0;i<8;i++){
-            if(row<grid.length && row>-1 && col<grid.length && col>-1){
+        if(document.getElementById(surround[0].join("")).classList.contains("black")||document.getElementById(surround[1].join("")).classList.contains("black")||document.getElementById(surround[2].join("")).classList.contains("black")||document.getElementById(surround[3].join("")).classList.contains("black")||document.getElementById(surround[4].join("")).classList.contains("black")||document.getElementById(surround[5].join("")).classList.contains("black")||document.getElementById(surround[6].join("")).classList.contains("black")||document.getElementById(surround[7].join("")).classList.contains("black")){
+            grid[row][col] = 1;
+            for(i=0;i<8;i++){
+                if(row<grid.length && row>-1 && col<grid.length && col>-1){
                 count = 0;
                 chRow = checkRow[i];
                 chCol = checkCol[i];
@@ -155,30 +125,68 @@ $(".box").click(function(){
                 newCol = col + chCol;
                     for(j=0;j<grid.length;j++){
                         if(newRow<grid.length && newRow>-1 && newCol<grid.length && newCol>-1){
-                            if(grid[newRow][newCol] == 1){
+                            if(grid[newRow][newCol] == 2){
                                 newRow += chRow;
                                 newCol += chCol;
                                 count++;
-                            } else if(grid[newRow][newCol] == 2){ 
+                            } else if(grid[newRow][newCol] == 1){ 
                                 if(count != 0){
                                     let nRow = newRow;
                                     let nCol = newCol;
                                     for(x=0;x<count;x++){
                                         nRow -= chRow;
                                         nCol -= chCol;
-                                        grid[nRow][nCol] = 2;
+                                        grid[nRow][nCol] = 1;
                                     }
                                 }   
-                            } else {
+                            } else if(grid[newRow][newCol] == 0){
                                 break;
                             }
                         }
-                        checkColor();    
                     }
-            }  
+                }   
+            }
+            checkColor();
+            changePlayer();
         }
-        checkColor();
-        changePlayer();
+    } else if (!current_player && !isOccupied) {
+        if(document.getElementById(surround[0].join("")).classList.contains("white")||document.getElementById(surround[1].join("")).classList.contains("white")||document.getElementById(surround[2].join("")).classList.contains("white")||document.getElementById(surround[3].join("")).classList.contains("white")||document.getElementById(surround[4].join("")).classList.contains("white")||document.getElementById(surround[5].join("")).classList.contains("white")||document.getElementById(surround[6].join("")).classList.contains("white")||document.getElementById(surround[7].join("")).classList.contains("white")){
+            grid[row][col] = 2;
+            for(i=0;i<8;i++){
+                if(row<grid.length && row>-1 && col<grid.length && col>-1){
+                    count = 0;
+                    chRow = checkRow[i];
+                    chCol = checkCol[i];
+                    let newRow = 0;
+                    let newCol = 0;
+                    newRow = row + chRow;
+                    newCol = col + chCol;
+                        for(j=0;j<grid.length;j++){
+                            if(newRow<grid.length && newRow>-1 && newCol<grid.length && newCol>-1){
+                                if(grid[newRow][newCol] == 1){
+                                    newRow += chRow;
+                                    newCol += chCol;
+                                    count++;
+                                } else if(grid[newRow][newCol] == 2){ 
+                                    if(count != 0){
+                                        let nRow = newRow;
+                                        let nCol = newCol;
+                                        for(x=0;x<count;x++){
+                                            nRow -= chRow;
+                                            nCol -= chCol;
+                                            grid[nRow][nCol] = 2;
+                                        }
+                                    }   
+                                } else {
+                                    break;
+                                }
+                            }    
+                        }
+                }  
+            }
+            checkColor();
+            changePlayer();
+        }
     }
 });
 
